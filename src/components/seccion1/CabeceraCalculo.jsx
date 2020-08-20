0bf3674b5306d3999ff5c6202c4d47d1f35bfda1
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-08-20 13:01:59 -0500

    cambio

--- src/components/seccion1/CabeceraCalculo.jsx
+++ src/components/seccion1/CabeceraCalculo.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 
 let CabeceraCalculo = ({nombre_clase,titulo}) => {
-    // let {
-    //     nombre_clase,
-    //     titulo
-    // } = props;
     return (
         <div className={nombre_clase}>
             <h1>{titulo}</h1>
